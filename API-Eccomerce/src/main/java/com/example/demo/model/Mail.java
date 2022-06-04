95cdeec0d17d3f356a6234205415008167b0fbfb
Breno Jardim Magrani <breno.jm25@gmail.com>
2022-06-04 10:06:00 -0300

    JavaMail implementado

---
+++ API-Eccomerce/src/main/java/com/example/demo/model/Mail.java
@@ -0,0 +1,64 @@
+package com.example.demo.model;
+
+public class Mail {
+	
+	private String from;
+	
+	private String to;
+	
+	private String subject;
+	
+	private String text;
+
+	public Mail(String from, String to, String subject, String text) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.subject = subject;
+		this.text = text;
+	}
+	
+	
+
+	public Mail() {
+		super();
+	}
+
+
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+	
+	
+}
